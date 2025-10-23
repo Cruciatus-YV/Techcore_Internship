eee64fe2db82e6ff3feb5eb19c888a30fe919115
Cruciatus-YV <voronkin_yura@list.ru>
2025-10-23 18:41:19 +0300

    Add CancellationToken.Cancel usage demonstration

---
+++ Module_2/Task337_5_CancellationToken.cs
@@ -0,0 +1,38 @@
+﻿namespace Techcore_Internship.Module_2;
+
+public static class Task337_5_CancellationToken
+{
+    public static async Task Run()
+    {
+        Console.WriteLine("\nTask337.5 - CancellationToken\n");
+        using var cts = new CancellationTokenSource();
+        var token = cts.Token;
+        var task = Task.Run(() => DoWork(token), token);
+        Console.WriteLine("Нажмите любую клавишу для отмены задачи...");
+        Console.ReadKey();
+        Console.WriteLine();
+        cts.Cancel();
+        try
+        {
+            await task;
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("Задача была отменена.");
+        }
+        Console.WriteLine(new string('-', 30));
+    }
+    public static void DoWork(CancellationToken token)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            if (token.IsCancellationRequested)
+            {
+                Console.WriteLine("Была запрошена отмена");
+                token.ThrowIfCancellationRequested();
+            }
+            Console.WriteLine($"Выполнение работы... {i + 1}/10");
+            Thread.Sleep(500);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,36 +1,38 @@
 ﻿using Techcore_Internship.Module_2;
 
-// Module 1
+//// Module 1
 
-Task335_1_BasicOutput.Run();
+//Task335_1_BasicOutput.Run();
 
-Task335_2_BasicConstructs.Run();
+//Task335_2_BasicConstructs.Run();
 
-Task335_3_Calculator.Run();
-Task335_3_Calculator.Run2();
+//Task335_3_Calculator.Run();
+//Task335_3_Calculator.Run2();
 
-Task335_4_ArraysCollections.Run();
+//Task335_4_ArraysCollections.Run();
 
-Task335_5_LINQ.Run();
+//Task335_5_LINQ.Run();
 
-Task335_6_OOP.Run();
+//Task335_6_OOP.Run();
 
-Task335_7_RefValTypes.Run();
+//Task335_7_RefValTypes.Run();
 
-Task335_8_InheritancePolymorphism.Run();
+//Task335_8_InheritancePolymorphism.Run();
 
-Task335_9_Intarfaces.Run();
+//Task335_9_Intarfaces.Run();
 
-Task335_10_Record.Run();
+//Task335_10_Record.Run();
 
-//Module 2
+////Module 2
 
-Task337_1_ThreadVsTask.Run();
+//Task337_1_ThreadVsTask.Run();
 
-await Task337_2_AsyncAwait.Run();
+//await Task337_2_AsyncAwait.Run();
 
-Task337_3_StateMachine.Run();
+//Task337_3_StateMachine.Run();
 
-await Task337_4_TaskWhenAll.Run();
+//await Task337_4_TaskWhenAll.Run();
+
+await Task337_5_CancellationToken.Run();
 
 Console.ReadLine();
