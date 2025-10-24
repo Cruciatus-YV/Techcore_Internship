a2a990deda8638f14e3307a6df7c15fd806de63b
Cruciatus-YV <voronkin_yura@list.ru>
2025-10-24 18:27:21 +0300

    Created webapi project, ApiController and HttpGet method

---
+++ Techcore_Internship.WebApi/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Techcore_Internship.WebApi.Controllers
+{
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+
+        [HttpGet("hello")]
+        public string Get()
+        {
+            return "Hello world";
+        }
+    }
+}
---
+++ Techcore_Internship.WebApi/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Services
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Techcore Internship API",
+        Version = "v1",
+        Description = "API для стажировки в Techcore"
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
